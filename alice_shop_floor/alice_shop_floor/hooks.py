41ec0595b70189785c2de259fe67fa9400fcdd9f
joeluvsfranko09-svg <joeluvsfranko09@gmail.com>
2026-06-05 08:47:35 -0500

    Create hooks.py

---
+++ alice_shop_floor/alice_shop_floor/hooks.py
@@ -0,0 +1,8 @@
+app_name = "alice_shop_floor"
+app_title = "Alice Shop Floor"
+app_publisher = "Athlettia LLC"
+app_description = "ALICE Shop Floor Layer"
+app_email = "frankoy@athlettia.com"
+app_license = "MIT"
+
+required_apps = ["frappe", "erpnext"]
